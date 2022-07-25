0b78a06a05b35ddbb5bd5cd4062f0fae4dec6743
Henry B <noelys215@gmail.com>
2022-07-24 20:14:36 -0400

    added generators examples

--- fun.js
+++ fun.js
@@ -77,3 +77,75 @@ const obj = test();
 .- isNan() checks whether value is Not-A-Number 0r not
 .- isFinite() checks for NaN as well as Infinity values
 */
+
+//* Arrow Functions
+/* 
+1. Explain the syntactical features of an arrow function
+.- can avoid return statement and parenthesis when having a single argument
+2. Why does "this" not work in an arrow function
+.- We already have a class keyword to deal with classes, so we don't need the function to act as a class;
+.- Will point to window object
+3- How can you handle argument object like functionality in an arrow function?
+.- By using rest parameter
+*/
+
+//* Closures
+/* 
+1. How can you access private variables or a function outside the scope?
+.- const outer = () => () => console.log('inner called');
+2. Explain closure advantages.
+.- Main advantage is that any private variable for a certain scope can be accessed, whilst keeping
+it private so that the variable is protected from accidental value changes.
+*/
+
+//* How can sum(5)(6) = 11?
+/* 
+const sum = (a) => (b) => a + b;
+const ans = sum(5)(6);
+
+1. What is function currying?
+.- A way to call inner functions where you can pass arguments partially or pass multiple arguments
+in a function but 1 argument at a time.
+2. What will this statement do? 
+const multi = (a) => (b) => (c) => a * b * c;
+.- You have a function with an inner function, inside an inner function with a return statement.
+Will return multiplied values of three parameters
+3. Explain practical usage of currying
+.- Currying solves various purposes like passing partial params or avoiding unwanted repetitions.
+*/
+
+//* Iterables & Iterators
+/* 
+1. What is the purpose of the iterator
+.- To make a data structure iterable thats not, i.e object literals 
+2. How do you create an iterator
+.- Symbol.iterator method must be implemented which returns an iterator object 
+and also have a next() method which returns the object
+3. Explain a practical use of an iterator
+.- The use of an iterator is not very prompt when used in the same scope.
+Useful in a Custom Data structure thats needs a "for of" loop; creating a pointer to use within the data structure
+*/
+
+// * Generators
+/* 
+1. What are Generator functions, explain the syntax
+.- Generator functions gives you a way to pause and continue a process
+const gen = function* () {
+	console.log('Hello');
+	yield 'Yield Value';
+	console.log('World');
+	yield;
+	console.log('!');
+};
+const gObj = gen();
+console.log(gObj.next());
+
+2. Which is the right syntax. function* (){} or function *(){}
+.- Both work fine; doesn't matter; function* is common syntax
+3. Explain the three methods of generator functions
+.- next() moves pointer to next line, return() terminates function, throw() helps raise an error
+4. Explain use of yield*
+.- yield* is used to call generator function from another; recursion
+5. Can you prevent return() from terminating the generator?
+.- Yes, via try catch; switch cases or if statements
+*/
