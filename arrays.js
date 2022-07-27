345d2a01f7ea8ab00f497103296755e416acb0c4
Henry B <noelys215@gmail.com>
2022-07-26 21:16:56 -0400

    add arrays

---
+++ arrays.js
@@ -0,0 +1,11 @@
+//* Array Traversal
+let arr = [4, 7, 9];
+arr.forEach((el, i) => console.log(el, i));
+
+/* 
+?1. What is the difference between for in and for of
+.- For in displays index, for of displays values
+?2. Whats the output? 
+? let arr = [7,0,9]; console.log(arr[arr.length])
+.- undefined, theres no 3 index.
+*/
