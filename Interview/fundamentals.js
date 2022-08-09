66e09a2de50e64b2f31e4bb6a908aa2a64a8a41b
Henry B <noelys215@gmail.com>
2022-08-09 14:50:05 -0400

    add examples

--- Interview/fundamentals.js
+++ Interview/fundamentals.js
@@ -127,9 +127,9 @@ let completeOrder = (...args) => {
 	console.log(`Order #${[...args].toString()} completed`);
 };
 
-completeOrder = processOrder(completeOrder);
-completeOrder = addCustomer(completeOrder);
-completeOrder('23');
+// completeOrder = processOrder(completeOrder);
+// completeOrder = addCustomer(completeOrder);
+// completeOrder('23');
 
 //* Array
 const user = ['Erin'];
@@ -139,3 +139,256 @@ const append = (arr, el) => [...arr, el];
 const newUser = append(user, 'Hank');
 // console.log(user);
 // console.log(newUser);
+
+//
+//* Write a function which can concatenate 2 arrays
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arr3 = [7, 8, 9];
+
+function merge(arr1, arr2, arr3) {
+	return [...arr1, ...arr2, ...arr3];
+}
+
+//* Check that user with such name exists in array of objects
+
+const users1 = [
+	{
+		id: 1,
+		name: 'Jack',
+		isActive: true,
+	},
+	{
+		id: 2,
+		name: 'John',
+		isActive: true,
+	},
+	{
+		id: 3,
+		name: 'Mike',
+		isActive: false,
+	},
+];
+
+function existingUser(obj, name, exist = false) {
+	obj.forEach((user) => (user.name === name ? (exist = true) : exist));
+	return exist;
+}
+
+// console.log(existingUser(users1, 'Jack'));
+
+const isNameExists = (name, arr) => arr.some((el) => el.name === name);
+// console.log(isNameExists('Jack', users1));
+
+// Remove all duplicates in array
+
+const num = [1, 2, 3, 4, 4, 4];
+
+const dupe = (arr) => [...new Set(arr)];
+
+const uniqueArr = (arr) => {
+	const res = [];
+	arr.forEach((el) => {
+		if (!res.includes(el)) {
+			res.push(el);
+		}
+	});
+	return res;
+};
+
+// Sort the array of numbers
+// Sort array of objects by author's lastname
+
+const books = [
+	{ name: 'Harry Potter', author: 'Joanne Rowling' },
+	{ name: 'Warcross', author: 'Marie Lu' },
+	{ name: 'The Hunger Games', author: 'Suzanne Collins' },
+];
+
+const uniqueArr2 = (arr) => {
+	return arr.reduce((acc, el) => (acc.includes(el) ? acc : [...acc, el]), []);
+};
+
+const num2 = [1, 3, 2, 5, 4, 0];
+
+const res = num2.sort((a, b) => a - b);
+
+const sorted = books.sort((b1, b2) => {
+	const auth1 = b1.author.split(' ')[1];
+	const auth2 = b2.author.split(' ')[1];
+	return auth1 - auth2 ? 1 : -1;
+});
+
+//* Write a function which implements range
+
+function range(start, end) {
+	let res = [];
+	for (let i = start; i <= end; i++) {
+		res.push(i);
+	}
+	return res;
+}
+
+const fillArr = (start, end) => [...Array(end - start + 1).keys()].map((el) => el + start);
+
+function numRange(start, end) {
+	if (start === end) return [start];
+	return [start, ...range(start + 1, end)];
+}
+
+// console.log(numRange(1, 10));
+
+//* Write a function that implements shuffle
+
+const shuffle = (items) => {
+	return items
+		.map((item) => ({ sort: Math.random(), value: item }))
+		.sort((i1, i2) => i1.sort - i2.sort)
+		.map((a) => a.value);
+};
+
+const arr19 = [1, 2, 3, 4, 5, 6];
+
+arr19.sort(() => 0.5 - Math.random());
+
+function shuffleArray(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		let randomPosition = Math.floor(Math.random() * i + 1);
+		let temp = array[i];
+		//swap els
+		array[i] = array[randomPosition];
+		array[randomPosition] = temp;
+	}
+	return array;
+}
+
+//* Find the number of occurrences of min value in array
+const arr = [1, 2, 3, 1, 1, 1];
+const minVal = Math.min(...arr);
+
+const minArr = arr.filter((el) => el === minVal);
+
+console.log(minArr.length);
+
+// class Employee {
+// 	constructor(id, name) {
+// 		if (!id || !name) throw new Error('Employee ID and Name required');
+// 		this.id = id;
+// 		this.name = name;
+// 	}
+
+// 	setSalary(salary) {
+// 		this.salary = salary;
+// 	}
+
+// 	getSalary() {
+// 		return this.salary;
+// 	}
+
+// 	getId() {
+// 		return this.id;
+// 	}
+
+// 	getName() {
+// 		return this.name;
+// 	}
+// }
+
+// class Manager extends Employee {
+// 	setDepartment(department) {
+// 		this.department = department;
+// 	}
+// 	getDepartment() {
+// 		return this.department;
+// 	}
+// }
+
+// const newEmp = new Employee(569, 'Hank');
+// newEmp.setSalary(1000);
+// console.log(newEmp.getSalary());
+
+//* Class to Prototype
+
+var Employee = function (id, name) {
+	if (!id || !name) throw new Error('Employee ID and Name required');
+	this.id = id;
+	this.name = name;
+};
+
+Employee.prototype.setSalary = function (salary) {
+	this.salary = salary;
+};
+
+Employee.prototype.getSalary = function () {
+	return this.salary;
+};
+
+Employee.prototype.getId = function () {
+	return this.id;
+};
+
+Employee.prototype.getName = function () {
+	return this.name;
+};
+
+var Manager = function (params) {
+	Employee.apply(this, arguments);
+	// this.image = params.image;
+};
+
+Manager.prototype.setDepartment = function (department) {
+	this.department = department;
+};
+Manager.prototype.getDepartment = function () {
+	return this.department;
+};
+
+Manager.prototype = Object.create(Employee.prototype);
+Manager.prototype.constructor = Manager;
+
+//* Write debounce function
+// const saveInput = (name) => {
+// 	console.log('saveInput', name);
+// };
+
+// const debounce = (fn, timeout = 300) => {
+// 	let timer;
+// 	return (...args) => {
+// 		console.log('inner function', args);
+// 		clearTimeout(timer);
+// 		timer = setTimeout(() => {
+// 			fn.apply(this, args);
+// 		}, timeout);
+// 	};
+// };
+
+// const processChange = debounce(saveInput, 2000);
+// processChange('foo');
+// processChange('foo');
+// processChange('foo');
+
+//* Create throttle function
+
+const saveInput = (name) => {
+	console.log('saveInput', name);
+};
+
+const throttle = (fn, timeout = 300) => {
+	let isWaiting = false;
+	return (...args) => {
+		if (!isWaiting) {
+			fn.apply(this, args);
+			isWaiting = true;
+			setTimeout(() => {
+				isWaiting = false;
+			}, timeout);
+		}
+	};
+};
+
+const processChange = throttle(saveInput, 2000);
+processChange('foo');
+
+processChange('foo');
+processChange('foo');
